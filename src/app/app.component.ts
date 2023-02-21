99c33c1465412142cb0ece99f3fea87bc1e065cb
Thansingh <kamriyashiv@gmail.com>
2023-02-21 18:59:25 +0530

    hh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CrudApp';
+  data:{}={
+    name:'',
+    email:'',
+    mobile:'',
+    date:''
+  };
+  onSubmit(val:any){
+    this.data = val;
+    console.log(this.data);
+  }
 }
